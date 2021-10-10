5da66899ecd914085871681777bce8deebeca17c
shahnawazfakir <shahnawazfakir@gmail.com>
2021-10-10 01:44:46 -0400

    updates

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const api = {
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
-
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
